bc713d7472b73b8af9c20553c2ef00ace36f24d4
Sumeet <sumeet-bajwa-34@live.ca>
2022-07-27 21:07:56 -0700

    Update App.js
    add a local storage variable
    this saves all the todos we add to the list

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React,{useState, useRef} from "react";
+import React,{useState, useRef, useEffect} from "react";
 import ToDoList from "./ToDoList";
 import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   const [todos,setTodos] = useState([]);
   const todoNameRef = useRef();
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify)
+
+  }, [todos])
 
   function handleAddTodo(e){
     const name = todoNameRef.current.value
